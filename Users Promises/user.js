7e9e53d9e4f282c76e5d3056883e618bd22007f7
Eno <eno.meta@gmx.fr>
2022-10-03 17:13:19 +0200

    fetched and added to the html

---
+++ Users Promises/user.js
@@ -0,0 +1,27 @@
+const postsListEl = document.querySelector(".post-list");
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+}
+
+async function renderPost(id) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+    const postsDatas = await posts.json();
+    console.log(postsDatas)
+    postsListEl.innerHTML = postsDatas.map((post) => postHTML(post)).join("");
+}
+
+renderPost(id);
+
+function postHTML(post) {
+    return `<div class="post">
+                <div class="post__title">
+                    ${post.title}
+                </div>
+                <p class="post__body">
+                    ${post.body}
+                </p>
+            </div>`
+}
